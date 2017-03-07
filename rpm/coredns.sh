571f6e0a3d209d8ce0c14e81364409361bcddeac
tomsawyer <tomsawyer126@gmail.com>
2017-03-08 00:38:31 +0800

    modify rpm

--- rpm/coredns.sh
+++ rpm/coredns.sh
@@ -4,11 +4,18 @@ commit=$(git log| head -n 1 | awk '{printf $2}')
 sed -e "s/__commit__/$commit/" coredns.spec  > coredns-$commit.spec
 
 version=$(cat coredns.version)
-sed -i -e  "s/__version__/$version/" coredns-$commit.spec
+sed -e  "s/__version__/$version/" coredns-$commit.spec > coredns-$commit-${version}.spec
+rm coredns-$commit.spec 
 
-topdir=$(rpm --eval '${%_topdir}')
+which rpmbuild
+if [[ $? != 0 ]]; then
+    echo rpmbuild command not found
+    exit -1
+fi
+
+topdir=$(rpmbuild --eval '${%_topdir}')
 mkdir -p ${topdir}/{BUILD,BUILDROOT,SOURCES,RPMS,SRPMS,SPECS}
-mv coredns-$commit.spec ${topdir}/SPECS
+mv coredns-$commit-${version}.spec ${topdir}/SPECS
 
 #rpmbuild -ba rpmbuild/SPECS/coredns-$commit.spec 
 #
@@ -16,5 +23,7 @@ mv coredns-$commit.spec ${topdir}/SPECS
 #    echo "error exists, abort"
 #    exit -1
 #fi
+
+version=$(cat coredns.version | awk 'BEGIN{FS="."}{printf $1"."$2"."($3+1)}')
 
 #find rpmbuild/RPMS  -print0  -iname *.rpm | xargs  -0  mv  .
