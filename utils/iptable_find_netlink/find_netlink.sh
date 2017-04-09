effe80414a0acb13cf9e17ba47a3fb58361c6b0d
Administrator <heijinyuan@supercxp.com>
2017-04-09 14:54:08 +0800

    add util script && modify rpm

---
+++ utils/iptable_find_netlink/find_netlink.sh
@@ -0,0 +1,2 @@
+mysql -uroot iwg -e "select * from netlink_wl_view where ip_start_int<= inet_aton('$1') and inet_aton('$1') <= ip_end_int;"
+mysql -uroot iwg -e "select * from netlink_view where ip_start_int<= inet_aton('$1') and inet_aton('$1') <= ip_end_int;"
