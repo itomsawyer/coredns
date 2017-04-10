27ccc64115f93d4f60622f03fc494ad02a4c5db8
tomsawyer <tomsawyer126@gmail.com>
2017-04-10 13:23:45 +0800

    optimize in memory query times

--- middleware/vane/vane.go
+++ middleware/vane/vane.go
@@ -1,7 +1,6 @@
 package vane
 
 import (
-	"errors"
 	"math"
 	"net"
 	"time"
@@ -13,15 +12,6 @@ import (
 	"github.com/astaxie/beego/logs"
 	"github.com/miekg/dns"
 	"golang.org/x/net/context"
-)
-
-var (
-	errUnreachable     = errors.New("unreachable backend")
-	errUnexpectedLogic = errors.New("incorrect or incomplete data logic")
-	errFormatError     = errors.New("format error")
-
-	defaultMaxAttampts = 10
-	defaultTimeout     = 3 * time.Second
 )
 
 type Vane struct {
@@ -71,7 +61,6 @@ func (v *Vane) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (
 		clientSets      []engine.ClientSet
 		retcode, bestrc int
 		view            engine.View
-		policy          engine.Policy
 		err             error
 	)
 
@@ -113,7 +102,7 @@ func (v *Vane) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (
 		return dns.RcodeServerFailure, errUnexpectedLogic
 	}
 
-	v.Logger.Debug("use clientsets: %v", clientSets)
+	//v.Logger.Debug("use clientsets: %v", clientSets)
 	//clientSetID = clientSets[0].ID
 
 	// Get domainpool_id , if not found e.GetDomainPoolID return engine.DefaultDomainPoolID
@@ -127,194 +116,191 @@ try_again:
 	//v.Logger.Debug("use clientset_id : %d", clientSetID)
 	v.Logger.Debug("use domain pool id: %d", domain.DmPoolID)
 
-	found_policy := false
+	found_policy := make([]engine.Policy, 0, 2)
+	views := make([]engine.View, 0, 2)
 	for _, cs := range clientSets {
 		view, err = e.GetView(cs.ID, domain.DmPoolID)
 		if err != nil {
 			continue
 		}
+
+		views = append(views, view)
+
 		if view.Upstream == nil {
 			continue
 		}
 
 		//Policy is a method class to choose upstreamhost (ldns) from upstream (policy)
-		policy = view.Upstream.GetPolicy()
+		policy := view.Upstream.GetPolicy()
 		if policy == nil {
 			continue
 		}
 
 		//clientSetID = cs.ID
-		found_policy = true
-		break
+		found_policy = append(found_policy, policy)
 	}
 
-	if !found_policy {
+	// INSIST: len(found_policy) <= len(views) , we just care about found policy here
+	if len(found_policy) == 0 {
 		if _, domain, ok = rollback(engine.DefaultClientSetID, domain); ok {
 			goto try_again
 		}
 
-		v.Logger.Warn("view not found")
+		v.Logger.Warn("upstream policy not found")
 		return dns.RcodeServerFailure, errUnexpectedLogic
 	}
 
-	ex := NewExchangeHelper(view.Upstream, nil)
+	ex := NewExchangeHelper(nil)
 	ex.Timeout = v.UpstreamTimeout
 	bestrc = dns.RcodeServerFailure
 	replyMsg = nil
 	replys = nil
 
-	for i = 0; i < defaultMaxAttampts; i++ {
-		v.Logger.Debug("try select upstream hosts")
-		// for each time policy choose the next prior upstreamhosts group
-		uphosts := policy.Select()
-		if len(uphosts) == 0 {
-			// There no upstream host can be found , check out what we got.
-			break
-		}
+	for _, policy := range found_policy {
+		for i = 0; i < defaultMaxAttampts; i++ {
+			v.Logger.Debug("try select upstream hosts")
+			// for each time policy choose the next prior upstreamhosts group
+			uphosts := policy.Select()
+			if len(uphosts) == 0 {
+				// There no upstream host can be found , check out what we got.
+				break
+			}
 
-		if v.Debug {
 			for _, uh := range uphosts {
-				v.Logger.Debug("upstream host found %v", uh)
-			}
-		}
-
-		// Send dns query to every upstreamhost in uphosts, combine their response into slice replys
-		v.Logger.Debug("set upstream timeout: %s", v.UpstreamTimeout)
-		ex.Hosts = uphosts
-		ex.Timeout = v.UpstreamTimeout
-		replys, retcode = ex.DoExchange(ctx, state)
-		if v.Debug {
-			for _, r := range replys {
-				v.Logger.Debug("get reply from upstream host :\n%v", r)
-			}
-		}
-
-		if len(replys) == 0 {
-			continue
-		}
-
-		v.Logger.Debug("found retcode %d with bestrc cmp to current %d", retcode, bestrc)
-		if v.RcodePriority.PriorTo(retcode, bestrc) {
-			v.Logger.Debug("found better retcode: %d", retcode)
-			bestrc = retcode
-			replyMsg = replys[0]
-		}
-
-		if retcode != dns.RcodeSuccess {
-			continue
-		}
-
-		// No need to filter record with type is not A, Get a proper one to return
-		if q.Qtype != dns.TypeA {
-			if bestrc == dns.RcodeSuccess && replyMsg != nil {
-				w.WriteMsg(replyMsg)
-				return 0, nil
+				v.Logger.Debug("upstream host found %v", uh.Name)
 			}
 
-			continue
-		}
-
-		// better is the result set of all A that pass the filter with Route
-		better := make([]dns.RR, 0, 4)
-		rrset := rrSet{}
-		cnameSet := rrSet{}
-		for _, reply := range replys {
-			rrlist := reply.Answer
-			for _, rr := range rrlist {
-				switch r := rr.(type) {
-				case (*dns.A):
-					key := r.A.String()
-					v.Logger.Debug("Add A %s into rrset", key)
-					rrset.Add(key, r)
-				case (*dns.CNAME):
-					key := r.Hdr.Name
-					v.Logger.Debug("Add CNAME %s -> %s into cnameset", key, r.Target)
-					cnameSet.Add(key, r)
-				default:
+			// Send dns query to every upstreamhost in uphosts, combine their response into slice replys
+			v.Logger.Debug("set upstream timeout: %s", v.UpstreamTimeout)
+			ex.Hosts = uphosts
+			replys, retcode = ex.DoExchange(ctx, state)
+			if v.Debug {
+				for _, r := range replys {
+					v.Logger.Debug("get reply from upstream host :\n%v", r)
 				}
 			}
-		}
 
-		bestRoutePrio := math.MaxInt32
-		for _, cs := range clientSets {
-			tmpView, err := e.GetView(cs.ID, domain.DmPoolID)
-			if err != nil {
+			if len(replys) == 0 {
 				continue
 			}
 
-			for _, rr := range rrset {
-				var routes engine.RouteSlice
-				a := rr.(*dns.A)
-				netlinks := e.GetNetLinks(a.A)
-				v.Logger.Debug("ip %s found netLink: %v", a.A, netlinks)
+			if v.RcodePriority.PriorTo(retcode, bestrc) {
+				v.Logger.Debug("found better retcode: %d", retcode)
+				bestrc = retcode
+				replyMsg = replys[0]
+			}
 
-				for _, nl := range netlinks {
-					routes = e.GetRoute(tmpView.RouteSetID, domain.DmPoolID, nl.ID)
-					if len(routes) > 0 {
-						break
+			if retcode != dns.RcodeSuccess {
+				continue
+			}
+
+			// No need to filter record with type is not A, Get a proper one to return
+			if q.Qtype != dns.TypeA {
+				if bestrc == dns.RcodeSuccess && replyMsg != nil {
+					w.WriteMsg(replyMsg)
+					return 0, nil
+				}
+
+				continue
+			}
+
+			// better is the result set of all A that pass the filter with Route
+			better := make([]dns.RR, 0, 4)
+			rrset := rrSet{}
+			cnameSet := rrSet{}
+			for _, reply := range replys {
+				rrlist := reply.Answer
+				for _, rr := range rrlist {
+					switch r := rr.(type) {
+					case (*dns.A):
+						key := r.A.String()
+						v.Logger.Debug("Add A %s into rrset", key)
+						rrset.Add(key, r)
+					case (*dns.CNAME):
+						key := r.Hdr.Name
+						v.Logger.Debug("Add CNAME %s -> %s into cnameset", key, r.Target)
+						cnameSet.Add(key, r)
+					default:
 					}
 				}
+			}
 
-				if len(routes) == 0 {
-					v.Logger.Debug("no route found")
-					continue
-				}
+			bestRoutePrio := math.MaxInt32
+			for _, tmpView := range views {
+				for _, rr := range rrset {
+					var routes engine.RouteSlice
+					a := rr.(*dns.A)
+					netlinks := e.GetNetLinks(a.A)
+					v.Logger.Debug("ip %s found netLink: %v", a.A, netlinks)
 
-				route := routes[0]
-				v.Logger.Debug("route found to outlink %s", route.OutLink.Addr)
+					for _, nl := range netlinks {
+						routes = e.GetRoute(tmpView.RouteSetID, domain.DmPoolID, nl.ID)
+						if len(routes) > 0 {
+							break
+						}
+					}
 
-				if origDomain.Monitor {
-					v.Logger.Debug("domain %s with dmpool %d need to use dynamic monitor", q.Name, domain.DmPoolID)
-					status, ok := e.GetLink(a.A.String(), routes[0].OutLink.Addr)
-					if ok && status.Status == engine.LinkStatusDown {
-						v.Logger.Debug("%s dynamic monitor status down", a)
+					if len(routes) == 0 {
+						v.Logger.Debug("no route found")
 						continue
 					}
+
+					route := routes[0]
+					v.Logger.Debug("route found to outlink %s", route.OutLink.Addr)
+
+					if origDomain.Monitor {
+						v.Logger.Debug("domain %s with dmpool %d need to use dynamic monitor", q.Name, domain.DmPoolID)
+						status, ok := e.GetLink(a.A.String(), routes[0].OutLink.Addr)
+						if ok && status.Status == engine.LinkStatusDown {
+							v.Logger.Debug("%s dynamic monitor status down", a)
+							continue
+						}
+					}
+
+					if route.Priority > bestRoutePrio {
+						continue
+					}
+
+					if route.Priority < bestRoutePrio {
+						bestRoutePrio = route.Priority
+						better = better[:0]
+						v.Logger.Debug("better route found with priority %d", route.Priority)
+					}
+
+					v.Logger.Debug("ip addr %s has been accepted for %s", a.A, route.OutLink.Addr)
+					a.Hdr.Name = q.Name
+					better = append(better, a)
 				}
 
-				if route.Priority > bestRoutePrio {
-					continue
+				if len(better) > 0 {
+					//find at least one good  answer
+					v.Logger.Debug("clientset_id %d domainpool_id %d found routes", tmpView.ClientSetID, domain.DmPoolID)
+					break
 				}
-
-				if route.Priority < bestRoutePrio {
-					bestRoutePrio = route.Priority
-					better = better[:0]
-					v.Logger.Debug("better route found with priority %d", route.Priority)
-				}
-
-				v.Logger.Debug("ip addr %s has been accepted for %s", a.A, route.OutLink.Addr)
-				a.Hdr.Name = q.Name
-				better = append(better, a)
 			}
 
-			if len(better) > 0 {
-				//find at least one good  answer
-				v.Logger.Debug("clientset_id %d domainpool_id %d found routes", cs.ID, domain.DmPoolID)
-				break
+			if len(better) == 0 {
+				// nothing found, try next policy
+				continue
 			}
+
+			// we got answer, return
+			if cnameSlice := cnameSet.ToSlice(); len(cnameSlice) != 0 {
+				replyMsg.Answer = append(cnameSlice, better...)
+			} else {
+				replyMsg.Answer = better
+			}
+
+			if v.Debug {
+				v.Logger.Debug("Write anwser to client: \n%v", replyMsg)
+			}
+			w.WriteMsg(replyMsg)
+			return 0, nil
 		}
 
-		if len(better) == 0 {
-			// nothing found, try next policy
-			continue
+		if i == defaultMaxAttampts {
+			v.Logger.Warn("[%04d] policy upstream max attampts reached for client: %v domain: %s", warnPolicyMaxAttempts, cip, q.Name)
 		}
-
-		// we got answer, return
-		if cnameSlice := cnameSet.ToSlice(); len(cnameSlice) != 0 {
-			replyMsg.Answer = append(cnameSlice, better...)
-		} else {
-			replyMsg.Answer = better
-		}
-
-		if v.Debug {
-			v.Logger.Debug("Write anwser to client: \n%v", replyMsg)
-		}
-		w.WriteMsg(replyMsg)
-		return 0, nil
-	}
-
-	if i == defaultMaxAttampts {
-		v.Logger.Error("MaxAttampts of upstream query reached, configuration or policy maybe badly configured")
 	}
 
 	// try again the whole precedure with domainPoolID equals to
@@ -336,7 +322,7 @@ try_again:
 
 		// bestrc == dns.RcodeSuccess indicate that we got NOERROR response
 		// but no A record we want to pick, just keep slient to the client
-		v.Logger.Warn("vane cannot find a good answer for client: %v domain: %s", cip, q.Name)
+		v.Logger.Warn("[%04d] cannot find available answer for client: %v domain: %s", warnNoGoodAnswer, cip, q.Name)
 		replyMsg.Answer = nil
 		replyMsg.Ns = nil
 		w.WriteMsg(replyMsg)
@@ -344,7 +330,7 @@ try_again:
 	}
 
 	//we tried our best but still got nothing
-	v.Logger.Warn("vane cannot resolve any answer for client: %v domain: %s", cip, q.Name)
+	v.Logger.Warn("[%04d] cannot resolve any answer from upstream for client: %v domain: %s", warnNoneReplies, cip, q.Name)
 	return dns.RcodeServerFailure, errUnreachable
 }
 
