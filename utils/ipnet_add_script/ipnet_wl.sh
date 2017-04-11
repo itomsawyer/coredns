8195d80a920ffff704434ce395e1d6826984783b
Administrator <heijinyuan@supercxp.com>
2017-04-11 11:57:23 +0800

    add util scripts

---
+++ utils/ipnet_add_script/ipnet_wl.sh
@@ -0,0 +1,10 @@
+if [[ -z $1 ]]; then
+    echo "usage: $0 <clientset_id>"
+    exit -1
+fi
+
+while read line; do
+    mask=${line#*/}
+    ipnet=${line%/*}
+    echo "insert into ipnet_wl values(0, \"$ipnet\", inet_ntoa(inet_aton(\"$ipnet\") + pow(2, 32 - $mask) -1), \"$ipnet\", $mask, $1);"
+done
--- utils/iptable_find_netlink/find_netlink.sh
+++ utils/iptable_find_netlink/find_netlink.sh
@@ -1,2 +1,4 @@
+echo iptable_wl
 mysql -uroot iwg -e "select * from netlink_wl_view where ip_start_int<= inet_aton('$1') and inet_aton('$1') <= ip_end_int;"
+echo iptable
 mysql -uroot iwg -e "select * from netlink_view where ip_start_int<= inet_aton('$1') and inet_aton('$1') <= ip_end_int;"
